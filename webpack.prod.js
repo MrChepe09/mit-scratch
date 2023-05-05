69fb977e61da4938535fbcdb09cbf7c90facc6f1
Bhupinder Singh <khanujabhupinder09@gmail.com>
2023-05-06 03:36:47 +0530

    resolved bugs

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 process.env["NODE_ENV"] = "production";
 
