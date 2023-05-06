148f1849e393d9d1578fea46a1b06e6d1efb2910
Bhupinder Singh <khanujabhupinder09@gmail.com>
2023-05-06 12:05:42 +0530

    build dir changed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 process.env["NODE_ENV"] = "production";
 
@@ -7,5 +8,13 @@ module.exports = merge([
   common,
   {
     mode: "production",
+    optimization: {
+      minimize: true,
+      minimizer: [
+        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+        // `...`,
+        new CssMinimizerPlugin(),
+      ],
+    },
   },
 ]);
